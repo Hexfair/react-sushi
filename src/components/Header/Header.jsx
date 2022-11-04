2e9c078672beeff61080d6891cf93097d6def203
Hexfair <Hexfair17@gmail.com>
2022-11-03 22:48:25 -0400

    Commit 10 - html cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,13 +11,15 @@ const Header = () => {
 					<img src={logo} alt='Логотип' />
 				</Link>
 				<div className='header__title'>SUSHI PARTY</div>
-				<button className='header__button'>
-					<svg className='header__button--cart' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#756868" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-						<circle cx="9" cy="21" r="1"></circle>
-						<circle cx="20" cy="21" r="1"></circle>
-						<path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
-					<span className='header__button--price'>1200 р.</span>
-				</button>
+				<Link to="/cart">
+					<button className='header__button'>
+						<svg className='header__button--cart' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#756868" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+							<circle cx="9" cy="21" r="1"></circle>
+							<circle cx="20" cy="21" r="1"></circle>
+							<path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+						<span className='header__button--price'>1200 р.</span>
+					</button>
+				</Link>
 			</div>
 		</div>
 	)
