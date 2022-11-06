f1770fa01f9a1aa5636a97b7783aa33694ddb1d2
Hexfair <hexfair17@gmail.com>
2022-11-06 17:20:39 -0500

    Commit 14 - before TypeScript

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../assets/logo.png";
 import "./Header.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 //=========================================================================================================================
 
@@ -19,6 +19,8 @@ const Header = () => {
 		isMounted.current = true;
 	}, [items]);
 
+	const { pathname } = useLocation();
+
 	return (
 		<div className='header'>
 			<div className='header__body'>
@@ -26,15 +28,17 @@ const Header = () => {
 					<img src={logo} alt='Логотип' />
 				</Link>
 				<div className='header__title'>SUSHI PARTY</div>
-				<Link to="/cart">
-					<button className='header__button'>
-						<svg className='header__button--cart' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#756868" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-							<circle cx="9" cy="21" r="1"></circle>
-							<circle cx="20" cy="21" r="1"></circle>
-							<path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
-						<span className='header__button--price'>{totalPrice} р.</span>
-					</button>
-				</Link>
+				{pathname !== '/cart' &&
+					<Link to="/cart">
+						<button className='header__button'>
+							<svg className='header__button--cart' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#756868" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+								<circle cx="9" cy="21" r="1"></circle>
+								<circle cx="20" cy="21" r="1"></circle>
+								<path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+							<span className='header__button--price'>{totalPrice} р.</span>
+						</button>
+					</Link>
+				}
 			</div>
 		</div>
 	)
