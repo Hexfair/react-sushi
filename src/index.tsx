c23573a9f69031bc3edc1ed1a6b283c95ee9964b
Hexfair <Hexfair17@gmail.com>
2022-11-08 18:33:23 -0500

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 //=========================================================================================================================
 
 const rootElem = document.getElementById('root');
@@ -11,11 +11,11 @@ const rootElem = document.getElementById('root');
 if (rootElem) {
 	const root = ReactDOM.createRoot(rootElem);
 	root.render(
-		<BrowserRouter>
+		<HashRouter basename={process.env.PUBLIC_URL}>
 			<Provider store={store}>
 				<App />
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
