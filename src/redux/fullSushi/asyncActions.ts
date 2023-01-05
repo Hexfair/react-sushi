107cf02420275cc312b85095c7145a4a0d7d6596
Hexfair <Hexfair17@gmail.com>
2023-01-05 16:36:38 -0500

    Readme

--- src/redux/fullSushi/asyncActions.ts
+++ src/redux/fullSushi/asyncActions.ts
@@ -6,7 +6,7 @@ import { SushiItemType } from "../sushi/types";
 export const fetchFullSushi = createAsyncThunk(
 	'fullSushi/fetchFullSushiStatus',
 	async (params: string) => {
-		const { data } = await axios.get<SushiItemType>(`https://6359b1f538725a1746b65927.mockapi.io/sushi/${params}`);
+		const { data } = await axios.get<SushiItemType>(`https://633b5933c1910b5de0c41000.mockapi.io/sushi/${params}`);
 		return data;
 	}
 )
--- src/redux/sushi/asyncActions.ts
+++ src/redux/sushi/asyncActions.ts
@@ -7,7 +7,7 @@ export const fetchSushies = createAsyncThunk(
 	'sushi/fetchSushiesStatus',
 	async (params: Record<string, string>) => {
 		const { categoryQuery, sortQuery, searchQuery } = params;
-		const { data } = await axios.get<SushiItemType[]>(`https://6359b1f538725a1746b65927.mockapi.io/sushi?${categoryQuery}${sortQuery}${searchQuery}`);
+		const { data } = await axios.get<SushiItemType[]>(`https://633b5933c1910b5de0c41000.mockapi.io/sushi?${categoryQuery}${sortQuery}${searchQuery}`);
 		return data;
 	}
 )
